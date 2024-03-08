d7b93ca3d67cbe53622a4b19a05b0cce796866bc
DanielBlake23 <daniel.blake23@students.bhasvic.ac.uk>
2024-03-08 15:39:04 +0000

    coldest dan

--- board1/src/Main.java
+++ board1/src/Main.java
@@ -1,10 +1,20 @@
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-   String[]board1={"start", "ant","mouse","rat","lobster","OWL","Badger", "Rabbit","chicken", "pig","fox","cow","bison","MISS A GO", "BUFFALO","Yak", "Leopard", "Hyena","Black Bear","Walrus","Elk", "Giraffe", "Lion", "Brown Bear","Elephant","Polar Bear"};
-        if (getPLayerPosition() == board1[1])){
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.valueOf(scanner.nextLine());
+        int dicetot = 0;
+        dicetot = getRoll1 + getRoll2;
+        if (number = "1"){
+            System.out.println("You rolled " + );
+        }
+        System.out.println("Press 1 to roll the dice : \n");
+
+   String[]board1={"start", "ant","mouse","rat","lobster","owl","Badger", "Rabbit","chicken", "pig","fox","cow","bison","miss a go", "buffalo","Yak", "Leopard", "Hyena","Black Bear","walrus","Elk", "Giraffe", "Lion", "Brown Bear","Elephant","Polar Bear"};
+        if (getPosition() == board1[1])){
             if (P1.isOwned() == FALSE){
                 System.out.println();("You landed on ant. If you want to buy it, it will cost you " + P1.PurchaseCost);
 
@@ -15,7 +25,7 @@ public class Main {
             }
 
         }
-        if (getPLayerPosition() == board1[2])){
+        if (getPosition() == board1[2])){
             if (P2.isOwned() == FALSE){
                 System.out.println();("You landed on mouse. If you want to buy it, it will cost you " + P2.PurchaseCost);
 
@@ -26,7 +36,7 @@ public class Main {
             }
 
         }
-        if (getPLayerPosition() == board1[3])){
+        if (getPosition() == board1[3])){
             if (P3.isOwned() == FALSE){
                 System.out.println();("You landed on rat. If you want to buy it, it will cost you " + P3.PurchaseCost);
 
@@ -37,7 +47,7 @@ public class Main {
             }
 
         }
-        if (getPLayerPosition() == board1[4])){
+        if (getPosition() == board1[4])){
             if (P4.isOwned() == FALSE){
                 System.out.println();("You landed on lobster. If you want to buy it, it will cost you " + P4.PurchaseCost);
 
@@ -48,7 +58,7 @@ public class Main {
             }
 
         }
-        if (getPLayerPosition() == board1[5])){
+        if (getPosition() == board1[5])){
             if (P5.isOwned() == FALSE){
                 System.out.println();("You landed on owl. If you want to buy it, it will cost you " + P5.PurchaseCost);
 
@@ -59,7 +69,7 @@ public class Main {
             }
 
         }
-        if (getPLayerPosition() == board1[6])){
+        if (getPosition() == board1[6])){
             if (P6.isOwned() == FALSE){
                 System.out.println();("You landed on Badger. If you want to buy it, it will cost you " + P6.PurchaseCost);
 
